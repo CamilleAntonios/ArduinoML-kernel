86a71d1eba67767a82834c0fc1c1fd671ff74f7d
Gauthier <gauthiermartin.public@gmail.com>
2025-11-24 11:45:02 +0100

    feat: digital value is not an expression longer, replaced with "digitalequal"

--- kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/structural/expressions/DigitalValue.java
+++
@@ -1,8 +0,0 @@
-package io.github.mosser.arduinoml.kernel.structural.expressions;
-
-public class DigitalValue implements Expression{
-    private boolean value;
-    public DigitalValue(boolean value) {
-        this.value = value;
-    }
-}

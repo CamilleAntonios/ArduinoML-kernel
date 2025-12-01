6b74cc0cb6aff25e5c5dbb8a9e7b887e20d40991
Gholab <hajarelgholabzouri@gmail.com>
2025-12-01 11:34:41 +0100

    update to java 17, no compiler error

---
+++ embeddeds/groovy/Switch.ino
@@ -0,0 +1,37 @@
+// Generated using run.sh with Switch.groovy by dsl-groovy-all.jar on Mon Dec  1 10:57:48     2025
+// Wiring code generated from an ArduinoML model
+// Application name: Switch!
+
+long debounce = 200;
+
+enum STATE {on, off};
+STATE currentState = off;
+
+boolean buttonBounceGuard = false;
+long buttonLastDebounceTime = 0;
+
+void setup(){
+  pinMode(9, INPUT);  // button [Sensor]
+  pinMode(12, OUTPUT); // led [Actuator]
+}
+
+void loop() {
+	switch(currentState){
+		case on:
+			digitalWrite(12,HIGH);
+			buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
+			if( digitalRead(9) == HIGH && buttonBounceGuard) {
+				buttonLastDebounceTime = millis();
+				currentState = off;
+			}
+		break;
+		case off:
+			digitalWrite(12,LOW);
+			buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
+			if( digitalRead(9) == HIGH && buttonBounceGuard) {
+				buttonLastDebounceTime = millis();
+				currentState = on;
+			}
+		break;
+	}
+}

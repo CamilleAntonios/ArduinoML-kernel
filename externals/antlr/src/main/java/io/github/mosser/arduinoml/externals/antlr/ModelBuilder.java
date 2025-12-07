8f75fd389c87304efbcd988baaba952790c2728e
Gauthier <gauthiermartin.public@gmail.com>
2025-12-07 15:50:43 +0100

    feat: it is now impossible to compare two constants. Checked at runtime

--- externals/antlr/src/main/java/io/github/mosser/arduinoml/externals/antlr/ModelBuilder.java
+++ externals/antlr/src/main/java/io/github/mosser/arduinoml/externals/antlr/ModelBuilder.java
@@ -8,7 +8,7 @@ import io.github.mosser.arduinoml.kernel.App;
 import io.github.mosser.arduinoml.kernel.behavioral.*;
 import io.github.mosser.arduinoml.kernel.structural.actuators.AnalogActuator;
 import io.github.mosser.arduinoml.kernel.structural.actuators.DigitalActuator;
-import io.github.mosser.arduinoml.kernel.structural.expressions.DigitalEqualOperation;
+import io.github.mosser.arduinoml.kernel.structural.expressions.digitalbinaryoperations.DigitalEqualOperation;
 import io.github.mosser.arduinoml.kernel.structural.expressions.Expression;
 import io.github.mosser.arduinoml.kernel.structural.expressions.NotOperation;
 import io.github.mosser.arduinoml.kernel.structural.expressions.analogbinaryoperations.*;
--- kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/structural/expressions/analogbinaryoperations/AnalogBinaryOperation.java
+++ kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/structural/expressions/analogbinaryoperations/AnalogBinaryOperation.java
@@ -3,12 +3,19 @@ package io.github.mosser.arduinoml.kernel.structural.expressions.analogbinaryope
 import io.github.mosser.arduinoml.kernel.generator.Visitor;
 import io.github.mosser.arduinoml.kernel.structural.expressions.Expression;
 import io.github.mosser.arduinoml.kernel.structural.signals.AnalogSignal;
+import io.github.mosser.arduinoml.kernel.structural.signals.AnalogSignalConstant;
+import io.github.mosser.arduinoml.kernel.structural.signals.DigitalSignalConstant;
 
 public abstract class AnalogBinaryOperation implements Expression {
     protected AnalogSignal left;
     protected AnalogSignal right;
 
     public AnalogBinaryOperation(AnalogSignal left, AnalogSignal right) {
+        if (left instanceof AnalogSignalConstant && right instanceof AnalogSignalConstant) {
+            //Nous avons fait le choix de coder cela avec un check de type dynamique, car
+            //effectuer cette vérification avec un typage statique créerait beaucoup de duplication de classes.
+            throw new IllegalArgumentException("Arguments of a binary analog operation cant be both constants !");
+        }
         this.left = left;
         this.right = right;
     }
